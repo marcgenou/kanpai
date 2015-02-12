cbcec34ce17045c4d9c5703228ce9cda3d75f58e
Gonzalo Cámara <gonzalo.camara@betybyte.com>
2015-02-12 11:27:44 +0100

    bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,3 +8,9 @@ debconf-set-selections <<< 'mysql-server mysql-server/root_password password kan
 debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password kanpai'
 apt-get install -y ruby2.2 ruby2.2-dev curl git libmysqlclient-dev mysql-server nodejs apache2
 gem install rails --version 4.2.0 --no-ri --no-rdoc
+cd /vagrant/kanpai
+bundle install
+mysql -u root -pkanpai -e "create database kanpai_development"
+mysql -u root -pkanpai -e "create database kanpai_test"
+mysql -u root -pkanpai -e "create database kanpai_production"
+rails server -b 0.0.0.0
