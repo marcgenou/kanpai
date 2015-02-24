113a837b62b2fb68ba6ee45907bb118998cdbce7
gonzalo <gonzalo.camara@sistemas23.betybyte.local>
2015-02-24 18:19:53 +0100

    geocoder test

--- kanpai/app/controllers/showroom_controller.rb
+++ kanpai/app/controllers/showroom_controller.rb
@@ -2,4 +2,12 @@ class ShowroomController < ApplicationController
   def index
   @experiences = Experience.order(:title)
   end
+
+  def lookup_ip_location
+    if Rails.env.development?
+      Geocoder.search(request.remote_ip).first.country
+    else
+      request.location
+    end
+  end
 end
