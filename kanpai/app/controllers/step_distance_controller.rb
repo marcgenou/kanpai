302ff172068d690bb1735045958086bbd473734a
gon <gon@okachimachi.(none)>
2015-03-07 00:50:15 +0100

    step_distance city_name

--- kanpai/app/controllers/step_distance_controller.rb
+++ kanpai/app/controllers/step_distance_controller.rb
@@ -1,5 +1,11 @@
 class StepDistanceController < ApplicationController
   def index
-  	puts params.inspect
+  env['HTTP_X_REAL_IP'] = '83.56.16.252' if Rails.env.development?
+  location = request.location
+  	if params[:city_name]
+  		@city_name ||= params[:city_name]
+  	else
+  		@city_name = request.location.city
+  	end
   end
 end
