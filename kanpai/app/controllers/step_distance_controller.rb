7fce63e6db8f652d0573af33aae0dbd36f6456d3
gonzalo <gonzalo.camara@sistemas23.betybyte.local>
2015-03-09 14:41:00 +0100

    near 20 km

--- kanpai/app/controllers/step_distance_controller.rb
+++ kanpai/app/controllers/step_distance_controller.rb
@@ -2,10 +2,11 @@ class StepDistanceController < ApplicationController
   def index
   env['HTTP_X_REAL_IP'] = '83.56.16.252' if Rails.env.development?
   location = request.location
-  	if params[:city_name]
-  		@city_name ||= params[:city_name]
-  	else
+  	if params[:city_name].blank?
   		@city_name = request.location.city
+  	else
+  		@city_name ||= params[:city_name]
   	end
+  	@locations = Experience.near(@city_name, 20, :units => :km)
   end
 end
