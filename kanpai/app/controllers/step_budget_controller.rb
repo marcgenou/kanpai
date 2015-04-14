8814cf6dbbff20d78a542a2ca8a49a68d76a4a77
gonzalo <gonzalo@ueno.(none)>
2015-04-14 20:19:52 +0200

    budget

--- kanpai/app/controllers/step_budget_controller.rb
+++ kanpai/app/controllers/step_budget_controller.rb
@@ -1,5 +1,13 @@
 class StepBudgetController < ApplicationController
   def index
-  	@local_budget = Experience.find(params[:experience])
+  	@mychoice = Experience.find(params[:experience])
+
+		@locations = Experience.near(@mychoice, 50, :units => :km)
+		@locations -= @mychoice
+
+
+  	@budget_travel = Experience.find(params[:experience])
+  	@medium_travel = Experience.find(params[:experience])
+  	@splurge_travel = Experience.find(params[:experience])  	
   end
 end
