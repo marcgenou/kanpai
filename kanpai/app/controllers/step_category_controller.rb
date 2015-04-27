57a7198197948a279f8868c9f6d3789414f51468
gon <gon@okachimachi.(none)>
2015-04-27 12:11:35 +0200

    step_category changes

--- kanpai/app/controllers/step_category_controller.rb
+++ kanpai/app/controllers/step_category_controller.rb
@@ -3,5 +3,8 @@ class StepCategoryController < ApplicationController
   	@city_name = params[:city_name]
   	@mychoice = Experience.find(params[:experience])
   	@mychoice_distance = params[:mychoice]
+
+
+  	@mychoice_distance_price = @mychoice_distance.to_f * params[:multiplier].to_f
   end
 end
