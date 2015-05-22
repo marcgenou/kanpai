c82374b894fb9fd0b24e227d459211976ad75330
gonzalo <gonzalo.camara@sistemas23.betybyte.local>
2015-05-22 14:15:01 +0200

    wip model

--- kanpai/app/models/experience.rb
+++ kanpai/app/models/experience.rb
@@ -7,16 +7,5 @@ class Experience < ActiveRecord::Base
 	geocoded_by :address
 	after_validation :geocode, if: ->(obj){ obj.address.present? and obj.address_changed? }
 
-	has_many :line_items
-	before_destroy :ensure_not_referenced_by_any_line_time
-	private
-	# ensure that there are no line items referencing this experience
-	def ensure_not_referenced_by_any_line_item
-		if line_items.empty?
-			return true
-		else
-			errors.add(:base, 'Line Items present')
-			return false
-		end
-	end
+	has_many :countries
 end
