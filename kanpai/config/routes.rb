104e0b6d238b0aab3301206c2ad7ec94229b5c43
gonzalo <gonzalo.camara@sistemas23.betybyte.local>
2015-05-26 18:08:02 +0200

    breadcumbs testing

--- kanpai/config/routes.rb
+++ kanpai/config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'showroom#index', as: 'showroom' 
+  root 'showroom#index'#, as: 'showroom' 
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
