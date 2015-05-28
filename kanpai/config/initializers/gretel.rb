aacd6e33d22db3a49b58f1eab97327d68d5cf297
gonzalo <gonzalo.camara@sistemas23.betybyte.local>
2015-05-28 17:02:42 +0200

    breadcumbs trails sidebar

---
+++ kanpai/config/initializers/gretel.rb
@@ -0,0 +1,3 @@
+Gretel::Trails.configure do |config|
+  config.store.secret = '81f73d28b4ae29b88e54e7812447cc3a4e4e9268704aa9781d29b14ace8e0a9eb014263a073fd898d8fd5b86f16949c8d8b8b554cafd89e78c5f192cf005c3b1' # Must be changed to something else to be secure
+end
